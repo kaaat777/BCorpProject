10a3e022373f2f97e7263b780464787d945fed47
kaaat777 <katherine.smith2026@gmail.com>
2025-02-05 10:15:40 -0500

    Updated the basic files needed for the project

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+
+    return render_template('results.html', query=query)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
---
+++ static/js/main.js
@@ -0,0 +1,4 @@
+
+
+console.log("Welcome to the B corp search website");
+
